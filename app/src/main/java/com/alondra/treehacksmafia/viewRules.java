b6d3a58f6f0f89f37cdafba3fcdb3417417a0b01
rmu2 <rmu@stanford.edu>
2015-02-21 17:08:33 -0800

    Change Rules from Activity to Fragment

--- app/src/main/java/com/alondra/treehacksmafia/viewRules.java
+++ app/src/main/java/com/alondra/treehacksmafia/viewRules.java
@@ -6,12 +6,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Rules extends ActionBarActivity {
+public class viewRules extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rules);
+        setContentView(R.layout.fragment_view_rules);
     }
 
 
